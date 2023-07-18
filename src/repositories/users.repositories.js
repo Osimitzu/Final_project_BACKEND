5c61785aaf3cc181e028f1b2a4221b82b72118d6
Osimitzu <angel_aocc@hotmail.com>
2023-07-17 19:05:53 -0600

    create car when the user is created

--- src/repositories/users.repositories.js
+++ src/repositories/users.repositories.js
@@ -1,8 +1,9 @@
-const { users } = require("../models");
+const { users, cars } = require("../models");
 
 const createNewUserREPO = async (newUser) => {
   const user = await users.create(newUser);
-  return user;
+  const car = await cars.create({ user_id: user.id });
+  return { user, car };
 };
 
 module.exports = {
