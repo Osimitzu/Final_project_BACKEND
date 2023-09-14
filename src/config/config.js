eaabb4a3d348289ceb3bb43cf9443306818802d2
Osimitzu <angel_aocc@hotmail.com>
2023-09-13 18:39:52 -0600

    deploy config

--- src/config/config.js
+++ src/config/config.js
@@ -20,10 +20,11 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_USERNAME_PRODUCTION,
+    password: process.env.DB_PASSWORD_PRODUCTION,
+    port: process.env.DB_PORT_PRODUCTION,
+    database: process.env.DB_NAME_PRODUCTION,
+    host: process.env.DB_HOST_PRODUCTION,
+    dialect: "postgres",
   },
 };
